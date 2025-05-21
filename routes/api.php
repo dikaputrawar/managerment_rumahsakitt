9e3380b981649aff655bc04ac545e185fe6c82f1
Rizki Febrian <541231191@student.smktelkom-pwt.sch.id>
2025-05-21 07:46:33 +0700

    PengambilanObat

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\InventoryController;  
 use App\Http\Controllers\Api\PaymentController;    
 use App\Http\Controllers\Api\PoliController;
+use App\Http\Controllers\Api\PengambilnObatController;
 
 Route::apiResource('poli', PoliController::class);
 
@@ -34,7 +35,9 @@ Route::apiResource('konsultasi', KonsultasiController::class);
 Route::apiResource('rekam-medis', RekamMedisController::class);
 Route::apiResource('laporan', LaporanController::class);
 Route::apiResource('inventory', InventoryController::class);
-Route::apiResource('payments', PaymentController::class);  
+Route::apiResource('payments', PaymentController::class);
+Route::apiResource('pengambilan-obat', PengambilanObatController::class);
+
 
 // Route default untuk user (sanctum auth)
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
